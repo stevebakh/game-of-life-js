1bf5d6a93cffe25d6ebe7a621fa1ef9e7625532b
Steven Bakhtiari <steven@substrate.co.uk>
2017-04-14 20:48:13 +0100

    Implement core game logic.
    Kill cells with too few or too many neighbours.
    Create living cell at coords surrounded by 3 neighbours.

--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -18,9 +18,26 @@ describe("In the Game of Life", () => {
                 expect(game.iterate()).toEqual([]);
             });
 
-            it("will survive with 2 direct neighbours", () => {
+            it("will survive with 2 neighbours", () => {
                 let game = new GameOfLife([{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}]);
-                expect(game.iterate()).toContain([{x: 1, y: 2}]);
+                expect(game.iterate()).toContain({x: 1, y: 2});
+            });
+
+            it("will survive with 3 neighbours", () => {
+                let game = new GameOfLife([{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 2, y: 2}]);
+                expect(game.iterate()).toContain({x: 1, y: 2});
+            });
+
+            it("will die with more than 3 neighbours", () => {
+                let game = new GameOfLife([{x: 1, y: 1}, {x: 1, y: 2}, {x: 1, y: 3}, {x: 2, y: 1}, {x: 2, y: 2}]);
+                expect(game.iterate()).not.toContain({x: 1, y: 2});
+            });
+        });
+
+        describe("a currently vacant set of coordinates", () => {
+            it("will give rise to a new living cell when there are exactly 3 neighbours", () => {
+                let game = new GameOfLife([{x: 1, y: 1}, {x: 1, y: 3}, {x: 2, y: 1}]);
+                expect(game.iterate()).toContain({x: 1, y: 2});
             });
         });
     });
